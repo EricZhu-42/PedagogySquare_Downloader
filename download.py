700633e581e41f6bffaf0c618b691e9d76052bb4
EricZhu-42 <zhuxinhao00@gmail.com>
2020-09-07 21:57:51 +0800

    Transfer all Selenium work to Requests (temp)
    重新设计了登录token获取方案，将所有由Selenium承担的部分转移到Requests，使得脚本运行不再需要配置Selenium

--- download.py
+++ download.py
@@ -6,11 +6,12 @@
 
 @Create date: 2020/03/31
 
-@Modified date: 2020/07/02
+@Modified date: 2020/09/07
 
 @description: A script to download file automatically from teaching.applysquare.com
 """
 
+import hashlib
 import json
 import logging
 import os
@@ -19,8 +20,13 @@ import time
 from contextlib import closing
 
 import requests
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+# Get Hex-md5 encoded password
+def hex_md5_stringify(raw_str:str):
+    md5_encoder = hashlib.md5()
+    md5_encoder.update(str(raw_str).encode('utf-8'))
+    return md5_encoder.hexdigest()
 
 # Function dealing with illegal characters of windows filename
 def filename_filter(name:str):
@@ -29,21 +35,19 @@ def filename_filter(name:str):
         name = name.replace(char, ' ')
     return name
 
-def construct_attchment_list(driver, token, pid, uid, cid):
+def construct_attchment_list(sess, token, pid, uid, cid):
     attachment_list = list()
     attachment_info_url = attachment_url_fmt.format(token, pid, 1, uid, cid)
-    driver.get(attachment_info_url)
-    raw_info = re.search(r'\{.*\}', driver.page_source).group(0)
-    info = json.loads(raw_info).get('message')
+    r = sess.get(attachment_info_url)
+    info = r.json()['message']
     file_num = info.get('count')
 
     current_page = 1
     # Add attachment path to attachment_list
     while len(attachment_list) < file_num:
         current_url = attachment_url_fmt.format(token, pid, current_page, uid, cid)
-        driver.get(current_url)
-        raw_info  = re.search(r'\{.*\}', driver.page_source).group(0)
-        info = json.loads(raw_info).get('message')
+        r = sess.get(current_url)
+        info = r.json()['message']
         attachment_list.extend(info.get('list'))
         current_page += 1
     return attachment_list
@@ -51,75 +55,39 @@ def construct_attchment_list(driver, token, pid, uid, cid):
 # Load config from config.json
 with open('config.json', 'r') as f:
     config = json.loads(f.read())
-
-user_name = config.get('username')
-user_passwd = config.get('password')
-headless_mode = config.get('headless_mode')
-download_all_ext = config.get('download_all_ext')
-download_all_courses = config.get('download_all_courses')
-ext_list = config.get('ext_list')
-ext_expel_list = config.get('ext_expel_list')
-cid_list = config.get('cid_list')
-
-# auto_restart = True
-# speed_threshold = 50 * 1024
+    user_name = config.get('username')
+    user_passwd = config.get('password')
+    headless_mode = config.get('headless_mode')
+    download_all_ext = config.get('download_all_ext')
+    download_all_courses = config.get('download_all_courses')
+    ext_list = config.get('ext_list')
+    ext_expel_list = config.get('ext_expel_list')
+    cid_list = config.get('cid_list')
 
 # Some metadata
-login_url = r"https://teaching.applysquare.com/Home/User/login"
+login_url = r"https://teaching.applysquare.com/Api/User/ajaxLogin"
 attachment_url_fmt = r'https://teaching.applysquare.com/Api/CourseAttachment/getList/token/{}?parent_id={}&page={}&plan_id=-1&uid={}&cid={}'
 course_info_url_fmt = r'https://teaching.applysquare.com/Api/Public/getIndexCourseList/token/{}?type=1&usertype=1&uid={}'
 attachment_detail_url_fmt = r'https://teaching.applysquare.com/Api/CourseAttachment/ajaxGetInfo/token/{}?id={}&uid={}&cid={}'
-token_pattern = r'(https://teaching\.applysquare\.com/Api/Public/getIndexCourseList/token/.*?)"'
 
-# Start the webdriver
-caps = DesiredCapabilities.CHROME
-caps['loggingPrefs'] = {'performance': 'ALL'}
-opt = webdriver.ChromeOptions()
-opt.add_experimental_option('w3c', False)
-opt.add_argument('log-level=3')
-if headless_mode:
-    opt.add_argument("--headless")
-driver = webdriver.Chrome(options=opt, desired_capabilities=caps)
+# Init Requests session
+sess = requests.Session()
 
-# Login to Pedagogy Square
-driver.get(login_url)
-time.sleep(1)
+# Login in
+print("Trying to log in, please wait ...")
+login_request = sess.post(login_url, data={"email" : user_name, "password" : hex_md5_stringify(user_passwd)})
 
-driver.find_element_by_xpath(r"/html/body/div[2]/div/div[2]/div/div/div/div/div[2]/div/div/div[1]/input").send_keys(user_name) # Send username
-driver.find_element_by_xpath(r'//*[@id="id_login_password"]').send_keys(user_passwd) # Send password
-driver.find_element_by_xpath(r'//*[@id="id_login_button"]').click() # Submit
-time.sleep(0.5)
+login_response = login_request.json()
+login_info = login_response['message']
 
-# Dealing with student-teacher selection
-try:
-    driver.find_element_by_xpath(r'/html/body/div[2]/div/div[2]/div/div/div[1]/div[2]/div[2]/div[1]/i').click() # Choose student
-    driver.find_element_by_xpath(r'/html/body/div[2]/div/div[2]/div/div/div[1]/div[4]/a').click() # Submit
-except Exception:
-    pass
-
-time.sleep(0.5)
-if (driver.current_url == r'https://teaching.applysquare.com/S/Index/index'):
-    print("Login Successfully!")
-else:
-    print("Login Error --- Please check your username & password")
-    print("Disable headless mode for detailed information")
-
-# Get token for authorization
-token = None
-while not token:
-    for entry in driver.get_log('performance'):
-        match_obj = re.search(token_pattern, entry.get('message'))
-        if match_obj:
-            temp_url = match_obj.group(1)
-            token = re.search(r'token/(.*?)\?', temp_url).group(1)
-            uid = re.search(r'uid=(.*?)', temp_url).group(1)
-            break
+token = login_info['token']
+uid = login_info['uid']
+print("Login successfully!")
 
 cid2name_dict = dict()
 course_info_url = course_info_url_fmt.format(token, uid)
-driver.get(course_info_url)
-raw_info = re.search(r'\{.*\}', driver.page_source).group(0)
-info = json.loads(raw_info).get('message')
+r = sess.get(course_info_url)
+info = r.json()["message"]
 for entry in info:
     cid2name_dict[entry.get('cid')] = entry.get('name')
 
@@ -143,7 +111,7 @@ for cid in cid_list:
         os.chdir("./{}".format(course_name))
 
     # Construct attachment list, with some dirs in it
-    course_attachment_list = construct_attchment_list(driver=driver, token=token, pid=0, uid=uid, cid=cid)
+    course_attachment_list = construct_attchment_list(sess=sess, token=token, pid=0, uid=uid, cid=cid)
 
     # Iteratively add files in dirs to global attachment list
     dir_counter = 0
@@ -152,7 +120,7 @@ for cid in cid_list:
             dir_counter += 1
             # Add dir content to attachment list
             dir_id = entry.get('id')
-            course_attachment_list.extend(construct_attchment_list(driver=driver, token=token, pid=dir_id, uid=uid, cid=cid))
+            course_attachment_list.extend(construct_attchment_list(sess=sess, token=token, pid=dir_id, uid=uid, cid=cid))
 
     print("Get {:d} files, with {:d} dirs".format(len(course_attachment_list)-dir_counter, dir_counter))
 
@@ -172,9 +140,8 @@ for cid in cid_list:
         # Get download url for un-downloadable files
         if (entry.get('can_download') == '0'):
             attachment_detail_url = attachment_detail_url_fmt.format(token, entry.get('id'), uid, cid)
-            driver.get(attachment_detail_url)
-            raw_info  = re.search(r'\{.*\}', driver.page_source).group(0)
-            info = json.loads(raw_info).get('message')
+            r = sess.get(attachment_detail_url)
+            info = r.json()['message']
             entry['path'] = info.get('path')
 
         with closing(requests.get(entry.get('path').replace('amp;', ''), stream=True)) as res:
